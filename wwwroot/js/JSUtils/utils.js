f80f869c11b7f5be8e257dfc6492c38dd7f5d834
Travis Rintoul <travis.rintoul@gmail.com>
2022-02-04 17:05:52 +1000

    Initial commit

---
+++ wwwroot/js/JSUtils/utils.js
@@ -0,0 +1,8 @@
+const Utils = {
+    rand: function (min, max)
+    {
+        return Math.floor((Math.random() * max) + min);
+    }
+};
+
+const U = Utils;
---
+++ wwwroot/js/site.js
@@ -0,0 +1,58 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () 
+{
+    /*
+    let canvas = ParticleMap("#landing-canvas");
+    canvas.ctx = canvas.$.get(0).getContext('2d');
+    
+    canvas.resize();
+    
+    canvas.width = $(window).width();
+    canvas.height = $(window).height();
+    
+    let particleLimit = (100 * window.devicePixelRatio);
+
+    for (let i=0; i < 100; i++)
+    {
+        canvas.addParticle(
+            U.rand(0, canvas.width),
+            U.rand(0, canvas.height)
+        );
+    }
+    
+    canvas.init();
+    console.log(canvas.particles);
+    
+    $(window).on('wheel', function (e) 
+    {
+        slider.onWheel(e);
+    });
+
+    $('#page').on('flick', function(e) 
+    {
+        if (e.direction < 0)
+        {
+            slider.next();
+        }
+        else 
+        {
+            slider.prev();
+        }
+    });
+    
+    $("#welcome-contact button").click(function () 
+    {
+        slider.activeSection = 'contact';    
+    });
+    */
+
+});
+
+/*
+window.onload = function() {
+    slider.onLoad();
+};
+*/
