8cd0bdbd015d7c25b79625b76123149e225096dc
Travis Rintoul <travis.rintoul@gmail.com>
2022-10-01 00:23:16 +1000

    Testing
    a

--- src/helpers/utils.ts
+++
@@ -1,8 +0,0 @@
-
-function numInRange(x: number, start: number, end: number)
-{
-    return x > start && x < end;
-}
-
-
-export { numInRange };
--- src/lib/Contact.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-
-</script>
-
-<div>
-    Contact Me
-</div>
